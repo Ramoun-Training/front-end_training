8fd24a083f7b42c5316d5fd7cf9e00a87ffd175b
Ramoun <omartarekramoun@gmail.com>
2020-07-19 05:11:31 -0400

    removed the changeImage() funcion

--- web-projects/test-project/scripts/script.js
+++ web-projects/test-project/scripts/script.js
@@ -15,19 +15,4 @@ function changeToRamoun() {
     }
 }
 
-function changeImage() {
-    let rose_image = "https://www.ikea.com/eg/en/images/products/smycka-artificial-flower-rose-red__0636963_PE698124_S5.JPG?f=s"
-    let odin_image = "images/og-logo-003bdac3098010b4f1143000a941b80c67eebd74fbc6f36b2e7ef92dd03e8020.png"
 
-    // let image = document.querySelector("img").src
-    // alert(image)
-    document.querySelector("img").src = rose_image
-    document.querySelector("img").click
-
-    if (image.src == odin_image) {
-        image.src = rose_image
-    } else {
-        image.src = odin_image
-    }
-
-}
