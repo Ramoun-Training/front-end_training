1d0ded28f4a9ed9a22fa74758888edb11e2afe1e
Ramoun <omartarekramoun@gmail.com>
2020-07-21 17:29:04 -0400

    added a bunch of functions to the main.js file

--- web-projects/test-project/scripts/main.js
+++ web-projects/test-project/scripts/main.js
@@ -1,7 +1,32 @@
-// https://theodinproject.com/sign_up?ref=homenav
+function openOdinSignupPage() {
+    a = document.querySelector("a") //global variable
+    let email = localStorage.getItem("email")
 
-function openOdinSignupPage(){
-    
+    if (!email) {
+        setEmail()
+    } else {
+        alert("Welcome, " + email)
+    }
+
+    a.href = "https://theodinproject.com/sign_up?ref=homenav"
+    a.click()
+    a.href = "https://theodinproject.com"
+}
+
+function setEmail() {
+    let email = prompt("Please, Enter your email:")
+
+    if (!email) {
+        setEmail()
+    } else {
+        const pattern = /\S+@\S+\.\S+/;
+        if (pattern.test(email)) {
+            localStorage.setItem("email", email)
+        } else {
+            prompt("Sorry, " + email + "is not a vaild e-mail.")
+            setEmail()
+        }
+    }
 }
 
 function changeToRamoun() {
@@ -9,21 +34,33 @@ function changeToRamoun() {
     const heading = document.querySelector('h1');
 
     if (heading.textContent == "The Odin Project") {
-        heading.textContent = "R}AM#UN>";    
+        heading.textContent = "R}AM#UN>";
     } else {
         heading.textContent = "The Odin Project"
     }
 }
 
-
 function changeImage() {
     img = document.querySelector('img');
-    let mysrc = img.src;
+    let mysrc = img.getAttribute("src");
     let imgsize = "800/420"
 
-    if (mysrc === "http://127.0.0.1:5500/web-projects/test-project/images/odin_logo.png") {
-        img.src = "https://placekitten.com/" + imgsize
+    if (mysrc === "images/odin_logo.png") {
+        img.src = "https://placeimg.com/" + imgsize + "/tech"
     } else {
-        img.src = "http://127.0.0.1:5500/web-projects/test-project/images/odin_logo.png"
+        img.src = "images/odin_logo.png" //img.setAttribute("src", "images/odin_logo.png") is the same thing
     }
+}
+
+function activateEasterEgg() {
+    const img = document.querySelector("img")
+    img.setAttribute("onclick", "changeImage()")
+    // img.onclick = changeImage() // doesn't work the way intended
+    img.style.cursor = "pointer"
+
+    const h1 = document.querySelector("h1")
+    h1.onclick = function () {
+        changeToRamoun()
+    }
+    h1.style.cursor = "pointer"
 }
